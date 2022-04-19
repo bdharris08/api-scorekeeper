cde801bd9b7c617c295d2dd954c76825a5940da1
Ben Harris <benhdev@gmail.com>
2022-04-19 18:48:29 -0400

    Add RDS

---
+++ terraform/rds.tf
@@ -0,0 +1,61 @@
+# RDS Aurora
+
+module "rds_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+
+  name        = "${var.app}-postgres"
+  description = "PostgreSQL security group"
+  vpc_id      = aws_vpc.main.id
+
+  # ingress
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 5432
+      to_port     = 5432
+      protocol    = "tcp"
+      description = "PostgreSQL access from within VPC"
+      cidr_blocks = aws_vpc.main.cidr_block
+    },
+  ]
+
+  tags = var.tags
+}
+
+module "rds" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "4.2.0"
+
+  identifier = "${var.app}-postgres"
+
+  engine               = "postgres"
+  engine_version       = "14.1"
+  family               = "postgres14" # DB parameter group
+  major_engine_version = "14"         # DB option group
+  instance_class       = "db.t4g.large"
+
+  allocated_storage     = 20
+  max_allocated_storage = 100
+
+  db_name  = var.app
+  username = "appuser"
+  port     = 5432
+
+  multi_az               = true
+  db_subnet_group_name   = aws_db_subnet_group.private_group.id
+  vpc_security_group_ids = [module.rds_security_group.security_group_id]
+
+  maintenance_window              = "Mon:00:00-Mon:03:00"
+  backup_window                   = "03:00-06:00"
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+  create_cloudwatch_log_group     = true
+
+  backup_retention_period = 0
+  skip_final_snapshot     = true
+  deletion_protection     = false
+
+  create_db_option_group    = false
+  create_db_parameter_group = false
+
+  tags = var.tags
+}
