382ce90cf0c104b75929a1082145610bdaba5c9c
Ben Harris <benhdev@gmail.com>
2022-04-19 17:56:26 -0400

    Fix fargate not being able to reach ecr

--- terraform/role.tf
+++ terraform/role.tf
@@ -10,7 +10,7 @@ data "aws_iam_policy_document" "app_role_assume_role_policy" {
 
     principals {
       type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
+      identifiers = ["ecs.amazonaws.com", "ecs-tasks.amazonaws.com"]
     }
   }
 }
