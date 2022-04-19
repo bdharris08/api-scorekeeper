6a93c52fdd0a3a3c6af84d58b2d5d445999fea60
Ben Harris <benhdev@gmail.com>
2022-04-18 22:39:26 -0400

    cert validation takes a long time

--- terraform/cert.tf
+++ terraform/cert.tf
@@ -26,4 +26,9 @@ resource "aws_route53_record" "cert_validation" {
 resource "aws_acm_certificate_validation" "main" {
   certificate_arn         = aws_acm_certificate.main.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+
+  # https://github.com/hashicorp/terraform-provider-aws/issues/9338
+  timeouts {
+    create = "60m"
+  }
 }
